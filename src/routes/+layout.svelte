c4ab150756f03cdac8cacb40ad777fecea577c13
Carlos <carlosmjr71@gmail.com>
2026-07-08 16:00:04 -0400

    hero section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,31 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import "./layout.css";
+	import favicon from "$lib/assets/favicon.svg";
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-
 <header>
-   <div>
-<p>MoonBeauty</p>
-<a href="/">Inicio</a>
-<a href="products">Productos</a> 
-<a href="contacts">Contactos</a>
-   </div>
+	<div>
+		<p>MoonBeauty</p>
+		<a href="/">Inicio</a>
+		<a href="products">Productos</a>
+		<a href="contacts">Contactos</a>
+	</div>
 
-   <div>
-
-
-   </div>
+	<div></div>
 </header>
 
 {@render children()}
 
-
 <footer>
- Mi Footer
+	<p>MoonBeauty</p>
+	<div>
+		<a href="https://www.instagram.com/moonbeauty.val/">Instagram</a>
+		<a href="">Facebook</a>
+		<a href="">Pinterest</a>
+	</div>
+	2024 MoonBeauty.Luminous Serenity for your skin.
 </footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,6 @@
-Esta es la ruta inicial
+<section>
+    <div>
+    <p>Nuestra Coleccion</p>
+    <p>Rituales de belleza inspirados en la serenidad luminosa. Descubre formulas clinicas envueltas en texturas etereas.</p>
+    </div>
+</section>
