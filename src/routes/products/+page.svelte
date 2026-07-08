0833da9e7e4ff7ef3ecad70c19c61ca33362cb4b
Carlos <carlosmjr71@gmail.com>
2026-07-08 16:41:24 -0400

    componentes

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,12 +1,14 @@
+<script>
+    import Tarjeta from "$lib/components/tarjeta.svelte";
+</script>
+
+<Tarjeta></Tarjeta>
+
 <section>
     <div>
-    <p>Nuestra Coleccion</p>
-    <p>Rituales de belleza inspirados en la serenidad luminosa. Descubre formulas clinicas envueltas en texturas etereas.</p>
-    </div>
-
-    <div>
-    <div>
-       <img src="/labial.png" alt="">
-    </div>
+        <p>Nuestra Coleccion</p>
+        <p>
+            Rituales de belleza inspirados en la serenidad luminosa. Descubre formulas clinicas envueltas en texturas etereas.
+        </p>
     </div>
 </section>
