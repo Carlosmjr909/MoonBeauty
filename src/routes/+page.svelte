77b2a289a050b8c455e03b3bec5146569467bd90
Carlos <carlosmjr71@gmail.com>
2026-07-10 14:57:24 -0400

    pagina inical section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,3 +6,38 @@
     <a href="/products">Ver Catalogo</a>
     </div>
 </section>
+
+<section>
+     <div>
+        <div>
+            <div>
+                <div>estrella</div>
+            <p>La Ciencia de la Calma</p>
+            <p>Seleccionamos cuidadosamente rituales coreanos que combinan la tecnologia clinica mas avanzada con ingredientes botanicos ancestrales</p>
+            <img src="/planta.png" alt="">
+            </div>
+            <div>
+                <div>Medalla</div>
+            <p>Curaduria Exclusiva</p>
+            <p>Solo marcas que cumplen con los estandares mas estrictos de K-Beauty y sostenibilidad</p>
+            </div>
+        </div>
+     </div>
+       
+     <div>
+        <div>
+         <div>
+            <div>Camion</div> 
+            <p>Envio Directo</p>
+            <p>Logistica optimizada para que tu ritual no se detenga. Directo desde Seul a tu puerta</p> 
+         </div>  
+         <div>
+            <img src="/agua.png" alt="">
+            <p>Luminous Serenity</p>
+            <p>Descubre el brillo que nace desde adentro con productos diseñados para nutrir no solo tu piel, sino tu bienestar diario</p>
+            <button>Saber Mas</button>
+         </div>  
+        </div>
+     </div>
+
+</section>
