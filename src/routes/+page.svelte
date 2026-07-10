656ec2da7e6a4c5eaa32106df4f5c6f8fe33c1b8
Carlos <carlosmjr71@gmail.com>
2026-07-10 15:05:54 -0400

    Cambio pagina inicial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,51 @@
 <section>
-<img src="/labial.png" alt="">
+    <img src="/labial.png" alt="" />
     <div>
-    <p>MoonBeauty</p>
-    <p>Desde Corea hasta tu hogar</p>
-    <a href="/products">Ver Catalogo</a>
+        <p>MoonBeauty</p>
+        <p>Desde Corea hasta tu hogar</p>
+        <a href="/products">Ver Catalogo</a>
     </div>
 </section>
 
 <section>
-     <div>
+    <div>
         <div>
-            <div>
-                <div>estrella</div>
+            <div>estrella</div>
             <p>La Ciencia de la Calma</p>
-            <p>Seleccionamos cuidadosamente rituales coreanos que combinan la tecnologia clinica mas avanzada con ingredientes botanicos ancestrales</p>
-            <img src="/planta.png" alt="">
-            </div>
-            <div>
-                <div>Medalla</div>
-            <p>Curaduria Exclusiva</p>
-            <p>Solo marcas que cumplen con los estandares mas estrictos de K-Beauty y sostenibilidad</p>
-            </div>
+            <p>
+                Seleccionamos cuidadosamente rituales coreanos que combinan la
+                tecnologia clinica mas avanzada con ingredientes botanicos
+                ancestrales
+            </p>
+            <img src="/planta.png" alt="" />
         </div>
-     </div>
-       
-     <div>
         <div>
-         <div>
-            <div>Camion</div> 
-            <p>Envio Directo</p>
-            <p>Logistica optimizada para que tu ritual no se detenga. Directo desde Seul a tu puerta</p> 
-         </div>  
-         <div>
-            <img src="/agua.png" alt="">
-            <p>Luminous Serenity</p>
-            <p>Descubre el brillo que nace desde adentro con productos diseñados para nutrir no solo tu piel, sino tu bienestar diario</p>
-            <button>Saber Mas</button>
-         </div>  
+            <div>Medalla</div>
+            <p>Curaduria Exclusiva</p>
+            <p>
+                Solo marcas que cumplen con los estandares mas estrictos de
+                K-Beauty y sostenibilidad
+            </p>
         </div>
-     </div>
+    </div>
 
+    <div>
+        <div>
+            <div>Camion</div>
+            <p>Envio Directo</p>
+            <p>
+                Logistica optimizada para que tu ritual no se detenga. Directo
+                desde Seul a tu puerta
+            </p>
+        </div>
+        <div>
+            <img src="/agua.png" alt="" />
+            <p>Luminous Serenity</p>
+            <p>
+                Descubre el brillo que nace desde adentro con productos
+                diseñados para nutrir no solo tu piel, sino tu bienestar diario
+            </p>
+            <button>Saber Mas</button>
+        </div>
+    </div>
 </section>
